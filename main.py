f819079a65c00c7cc71c028a1bf6c16ed1c0528c
Evan Condalary <econdalary@powerley.com>
2021-05-23 15:01:02 -0400

    Last cut of analysis with imputation, notes, some fixes

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import json
 import logging
+import warnings
 from pprint import pprint
 
 import pandas as pd
 from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.impute import SimpleImputer
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
 from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPClassifier
 from sklearn.preprocessing import MinMaxScaler
+
+warnings.filterwarnings("ignore")
 
 # set up logging
 logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
@@ -20,13 +24,19 @@ CAT_VARS = ['job', 'marital', 'education', 'default', 'housing', 'loan', 'contac
 NUM_VARS = ['age', 'campaign', 'pdays', 'previous', 'emp.var.rate',
        'cons.price.idx', 'cons.conf.idx', 'euribor3m', 'nr.employed']
 
-PRED_CUTOFF = 0.15  # best recall (both models)
-# PRED_CUTOFF = 0.5   # standard out
+PRED_CUTOFFS = [0.15, 0.25, 0.30, 0.35, 0.40, 0.50]
 
 
 def preprocess_data(df, categorical_cols, numeric_cols):
     # recode y as 0 or 1
     df["y"] = (df["y"] == "yes").astype("int")
+
+    # recode pdays so 999 = -1
+    df["pdays"] = df["pdays"].replace(999, -1)
+
+    # impute unknown categoricals
+    imp = SimpleImputer(missing_values="unknown", strategy='most_frequent')
+    df[categorical_cols] = imp.fit_transform(df[categorical_cols])
 
     # recode cat vars using data dict
     for var in categorical_cols:
@@ -71,31 +81,36 @@ def main():
     nn.fit(X, y)
 
     # predict on dataset
-    preds = {}
-    gb_prob = gb.predict_proba(X_test)[:,1]
-    preds["gradient_boost"] = (gb_prob > PRED_CUTOFF).astype("int")
-    nn_prob = nn.predict_proba(X_test)[:, 1]
-    preds["neural_net"] = (nn_prob > PRED_CUTOFF).astype("int")
+    naive_stat = (y_test == 0).sum() / y_test.count()
+    cutoff_scores = {}
+    for cutoff in PRED_CUTOFFS:
+        preds = {}
+        gb_prob = gb.predict_proba(X_test)[:,1]
+        preds["gradient_boost"] = (gb_prob > cutoff).astype("int")
+        nn_prob = nn.predict_proba(X_test)[:, 1]
+        preds["neural_net"] = (nn_prob > cutoff).astype("int")
 
-    # calculate scores for each model
-    scores = {}
-    for name, y_hat in preds.items():
-        metrics = {}
-        # scores
-        metrics["accuracy"] = accuracy_score(y_test, y_hat)
-        metrics["precision"] = precision_score(y_test, y_hat)
-        metrics["recall"] = recall_score(y_test, y_hat)
-        metrics["f1"] = f1_score(y_test, y_hat)
-        # confusion matrix
-        tn, fp, fn, tp = confusion_matrix(y_test, y_hat, labels=[0, 1]).ravel()
-        metrics["true_negatives"] = tn
-        metrics["false_positives"] = fp
-        metrics["false_negatives"] = fn
-        metrics["true_positives"] = tp
+        # calculate scores for each model
+        scores = {}
+        for name, y_hat in preds.items():
+            metrics = {}
+            # scores
+            metrics["accuracy"] = accuracy_score(y_test, y_hat)
+            metrics["precision"] = precision_score(y_test, y_hat)
+            metrics["recall"] = recall_score(y_test, y_hat)
+            metrics["f1"] = f1_score(y_test, y_hat)
+            metrics["passes_naive_test"] = metrics["accuracy"] > naive_stat
+            # confusion matrix
+            tn, fp, fn, tp = confusion_matrix(y_test, y_hat, labels=[0, 1]).ravel()
+            metrics["true_negatives"] = tn
+            metrics["false_positives"] = fp
+            metrics["false_negatives"] = fn
+            metrics["true_positives"] = tp
 
-        scores[name] = metrics
+            scores[name] = metrics
+            cutoff_scores[cutoff] = scores
 
-    pprint(scores)
+    pprint(cutoff_scores)
 
 
 if __name__ == "__main__":
